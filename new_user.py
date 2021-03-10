4729ad1acc5c18fabc05ab09db0293f72f7327e0
ThiagoPiccioni <thiagopiccioni@gmail.com>
2021-03-10 03:33:43 +0000

    general

---
+++ new_user.py
@@ -0,0 +1,13 @@
+
+class NewUser:
+
+    def __init__(self, playbrush_id, timestamp_utc, up_time, down_time, left_time, right_time, none_time):
+        self.playbrush_id = playbrush_id
+        self.timestamp_utc = timestamp_utc
+        self.up_time = up_time
+        self.down_time = down_time
+        self.left_time = left_time
+        self.right_time = right_time
+        self.none_time = none_time
+
+
